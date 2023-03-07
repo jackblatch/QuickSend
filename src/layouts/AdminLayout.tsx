84cf920b059ceaeccf419006e763c6774c161a38
jackb14 <jack@26385.xyz>
2023-03-07 11:59:41 +0000

    change colour of admin layout sidebar

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -17,6 +17,7 @@ import { useRouter } from "next/router";
 import Breadcrumbs from "~/components/Breadcrumbs";
 import { useSession } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import { format } from "path";
 
 const initialNavigation = [
   {
@@ -108,7 +109,7 @@ export default function AdminLayout({
                 leaveFrom="translate-x-0"
                 leaveTo="-translate-x-full"
               >
-                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-white">
+                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800">
                   <Transition.Child
                     as={Fragment}
                     enter="ease-in-out duration-300"
@@ -134,128 +135,126 @@ export default function AdminLayout({
                   </Transition.Child>
                   <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
                     <div className="flex flex-shrink-0 items-center px-4">
-                      <Logo justifyContent="justify-start" type="iconAndText" />
+                      <Logo
+                        justifyContent="justify-start"
+                        colorTheme="light"
+                        type="iconAndText"
+                      />
                     </div>
                     <nav className="mt-5 space-y-1 px-2">
                       {navigation.map((item) => (
-                        <Link
+                        <a
                           key={item.name}
                           href={item.href}
                           className={formatClasses(
                             item.current
-                              ? "bg-gray-100 text-gray-900"
-                              : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
                             "group flex items-center rounded-md px-2 py-2 text-base font-medium"
                           )}
                         >
                           <item.icon
                             className={formatClasses(
                               item.current
-                                ? "text-gray-500"
-                                : "text-gray-400 group-hover:text-gray-500",
+                                ? "text-gray-300"
+                                : "text-gray-400 group-hover:text-gray-300",
                               "mr-4 h-6 w-6 flex-shrink-0"
                             )}
                             aria-hidden="true"
                           />
                           {item.name}
-                        </Link>
+                        </a>
                       ))}
                     </nav>
                   </div>
-                  <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-                    <Link href="#" className="group block flex-shrink-0">
+                  <div className="flex flex-shrink-0 bg-gray-700 p-4">
+                    <a href="#" className="group block flex-shrink-0">
                       <div className="flex items-center">
-                        {/* <div>
-                          <img
-                            className="inline-block h-10 w-10 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
-                          />
-                        </div> */}
-                        <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-600 p-1 text-sm text-white">
+                        <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200 p-1 text-sm font-medium text-gray-700">
                           {`${session?.user?.firstName[0]}${session?.user?.lastName[0]}`}
                         </div>
                         <div className="ml-3">
-                          <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
+                          <p className="text-base font-medium text-white">
                             {`${session?.user?.firstName} ${session?.user?.lastName}`}
                           </p>
-                          <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+                          <p className="text-sm font-medium text-gray-400 group-hover:text-gray-300">
                             View profile
                           </p>
                         </div>
                       </div>
-                    </Link>
+                    </a>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
-              <div className="w-14 flex-shrink-0"></div>
+              <div className="w-14 flex-shrink-0">
+                {/* Force sidebar to shrink to fit close icon */}
+              </div>
             </div>
           </Dialog>
         </Transition.Root>
 
+        {/* Static sidebar for desktop */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-64 lg:flex-col">
-          <div className="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex min-h-0 flex-1 flex-col bg-gray-800">
             <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
               <div className="flex flex-shrink-0 items-center px-4">
-                <Logo justifyContent="justify-start" type="iconAndText" />
+                <Logo
+                  justifyContent="justify-start"
+                  colorTheme="light"
+                  type="iconAndText"
+                />
               </div>
-              <nav className="mt-5 flex-1 space-y-1 bg-white px-2">
+              <nav className="mt-5 flex-1 space-y-1 px-2">
                 {navigation.map((item) => (
-                  <Link
+                  <a
                     key={item.name}
                     href={item.href}
                     className={formatClasses(
                       item.current
-                        ? "bg-gray-100 text-gray-900"
-                        : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
                       "group flex items-center rounded-md px-2 py-2 text-sm font-medium"
                     )}
                   >
                     <item.icon
                       className={formatClasses(
                         item.current
-                          ? "text-gray-500"
-                          : "text-gray-400 group-hover:text-gray-500",
+                          ? "text-gray-300"
+                          : "text-gray-400 group-hover:text-gray-300",
                         "mr-3 h-6 w-6 flex-shrink-0"
                       )}
                       aria-hidden="true"
                     />
                     {item.name}
-                  </Link>
+                  </a>
                 ))}
               </nav>
             </div>
-            <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-              <Link href="#" className="group block w-full flex-shrink-0">
+            <div className="flex flex-shrink-0 bg-gray-700 p-4">
+              <a href="#" className="group block w-full flex-shrink-0">
                 <div className="flex items-center">
-                  <div>
-                    {/* <img
-                      className="inline-block h-9 w-9 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    /> */}
-                    <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-600 p-1 text-sm text-white">
-                      {`${session?.user?.firstName[0]}${session?.user?.lastName[0]}`}
-                    </div>
+                  <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200 p-1 text-sm font-medium text-gray-700">
+                    {`${session?.user?.firstName[0]}${session?.user?.lastName[0]}`}
                   </div>
                   <div className="ml-3">
-                    <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
+                    <p className="text-sm font-medium text-white">
                       {`${session?.user?.firstName} ${session?.user?.lastName}`}
                     </p>
-                    <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
+                    <p className="text-sm text-gray-400 group-hover:text-gray-300">
                       View profile
                     </p>
                   </div>
                 </div>
-              </Link>
+              </a>
             </div>
           </div>
         </div>
         <div className="flex flex-1 flex-col lg:pl-64">
-          <div className="sticky top-0 z-10 bg-white pl-1 pt-1 sm:pl-3 sm:pt-3 lg:hidden">
+          <div className="sticky top-0 z-10 bg-gray-100 pl-1 pt-1 sm:pl-3 sm:pt-3 lg:hidden">
             <button
               type="button"
-              className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+              className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
               onClick={() => setSidebarOpen(true)}
             >
               <span className="sr-only">Open sidebar</span>
