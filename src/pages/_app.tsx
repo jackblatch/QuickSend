30dcef91215deff955031b782b7fb3f47ba4354f
jackblatch <jack@26385.xyz>
2023-04-03 13:40:22 +0100

    feat: add plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: any = ({
   // AppType<{ session: Session | null }>
@@ -15,6 +16,13 @@ const MyApp: any = ({
 
   return getLayout(
     <SessionProvider session={session}>
+      <Head>
+        <script
+          defer
+          data-domain="quick-send.vercel.app"
+          src="https://plausible.io/js/script.js"
+        ></script>
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </SessionProvider>
