a84da6ed6b3811adc1b07fd1a47124b66e89714c
jackblatch <jack@26385.xyz>
2023-05-03 09:43:19 +0100

    update plausible domain

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,7 @@ const MyApp: any = ({
       <Head>
         <script
           defer
-          data-domain="quicksend.seperi.app"
+          data-domain="quicksend.jackblatch.com"
           src="https://plausible.io/js/script.js"
         ></script>
       </Head>
